a320b4cb212efc543d9cb226d336e63668f5e196
Ghanshyam Singh <way2gsn@gmail.com>
2026-03-11 17:31:29 +0530

    added dashboard

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname)));
 app.use(express.json());
 
 const { detectLanguage, t } = require("./lib/lang");
