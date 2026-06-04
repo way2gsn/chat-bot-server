fe38ea8ae70b77e15092b14defa7b8fb156ec4a6
Ghanshyam Singh <way2gsn@gmail.com>
2026-06-04 23:59:50 +0530

    added fix

--- scratch_test.js
+++ scratch_test.js
@@ -1,4 +1,31 @@
-const cat = require("./lib/catalog");
-console.log("Keys in catalog module:", Object.keys(cat));
-console.log("Is products defined?:", !!cat.products);
-console.log("Products count:", cat.products ? cat.products.length : "N/A");
+const https = require('https');
+
+const options = {
+  hostname: 'phasalbazar.up.railway.app',
+  port: 443,
+  path: '/admin/stats',
+  method: 'GET',
+  headers: {
+    'Authorization': 'Bearer phasal123'
+  }
+};
+
+const req = https.request(options, (res) => {
+  console.log('Status Code:', res.statusCode);
+  console.log('Headers:', res.headers);
+
+  let data = '';
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    console.log('Body:', data);
+  });
+});
+
+req.on('error', (e) => {
+  console.error('Error:', e);
+});
+
+req.end();
